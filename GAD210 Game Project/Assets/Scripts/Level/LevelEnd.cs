4cd063297946248142d0e07d7e089799c09ba11b
Bryce Standley <bryce@vectorpixel.net>
2019-11-06 12:23:56 +0800

    Level Complete UI
    Level complete ui and trigger for player

---
+++ GAD210 Game Project/Assets/Scripts/Level/LevelEnd.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelEnd : MonoBehaviour
+{
+
+    public GameObject blackFade;
+    public GameObject levelEndScreen;
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player"))
+        {
+            blackFade.SetActive(true);
+            blackFade.GetComponent<Animation>().Play("BlackFadeOut");
+            levelEndScreen.SetActive(true);
+        }
+    }
+
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
