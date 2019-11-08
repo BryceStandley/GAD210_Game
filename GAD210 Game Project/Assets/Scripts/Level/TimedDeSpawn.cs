c9e321d004c138e74ea1528757f130bb6a701f6f
Bryce Standley <bryce@vectorpixel.net>
2019-11-08 12:02:47 +0800

    Tutorial almost finished
    few more things to clean up, disable player input while cutscene plays, stop camera from clipping envionment, fix black fade and level reset

---
+++ GAD210 Game Project/Assets/Scripts/Level/TimedDeSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDeSpawn : MonoBehaviour
+{
+    public float destryTime = 5f;
+    private void Start() {
+        Invoke("DestroyItem", destryTime);  
+    }
+
+    private void DestroyItem()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ GAD210 Game Project/Assets/Scripts/UI/BlackFadeDisableEnable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackFadeDisableEnable : MonoBehaviour
+{
+    private float _fadeDisable = 0.5f;
+
+    private void Start() 
+    {
+        Invoke("DisableFade", _fadeDisable);
+    }
+
+    private void DisableFade()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
